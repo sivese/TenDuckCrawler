5a675d0d4a97d63ac20a42d5ab8e65c9e00303f7
Lee Tae Hwa <driversivese@gamil.com>
2018-12-07 09:04:42 +0900

    re commit

--- crawler.py
+++ crawler.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
 import queue
 
 class OtakuCrawler:
     def __init__(self):
         self.b = 10
         self.url = "https://www.pixiv.net/search.php?s_mode=s_tag&word=moe"
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('disable=gpu')
+
+        self.driver = webdriver.Chrome('./driver/chromedriver.exe', chrome_options=options)
+        self.driver.implicitly_wait(3)
         self.download_que = []
 
     #def searchImageByKeyword(self, keyword):
@@ -14,10 +22,10 @@ class OtakuCrawler:
         url = self.url + "&p=" + page
 
     def run(self):
-        req = requests.get(self.url)
-        html = req.text
+        self.driver.get(self.url)
+        html = self.driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        image_list = soup.select('body > div > div > div > section')
+        image_list = soup.select('body > div > div > div > section > div > div')
 
         for image in image_list:
             print(image)
